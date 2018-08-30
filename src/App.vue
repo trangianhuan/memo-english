8db093a3aeb73d7eb218924853df39b7000b8691
root <root@b120950-pc.framgia.com>
2018-08-30 22:41:04 +0700

    Add firebase into project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainApp/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainApp from './components/MainApp.vue'
+let config = {
+apiKey: "AIzaSyBa52ClshRaZashVd8U79FClWDBd2Rfi7o",
+authDomain: "demorealtimedatabase-a4ba2.firebaseapp.com",
+databaseURL: "https://demorealtimedatabase-a4ba2.firebaseio.com",
+projectId: "demorealtimedatabase-a4ba2",
+storageBucket: "demorealtimedatabase-a4ba2.appspot.com",
+messagingSenderId: "523187674313"
+};
+const appF = firebase.initializeApp(config);
+const dbF = appF.database();
+const post = dbF.ref('posts');
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        MainApp
+    },
+    data() {
+        return {
+            abc:null
+        }
+    },
+    methods: {
+        cancel() {
+            console.log('cancel nef')
+        }
+    },
+    computed: {
+
+    },
+    mounted(){
+        post.on('value', dataSnapshot => {
+            console.log(dataSnapshot.val())
+        });
+    }
 }
 </script>
 
---
+++ src/components/MainApp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        hello
+    </div>
+</template>
+<script>
+    export default{
+        name: 'MainAppNe',
+        props:{
+        }
+    }
+</script>
+<style lang="css"></style>
